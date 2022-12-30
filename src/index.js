21ab4427ebe8d520664e66e6816dc12344432577
arnav-sachdeva <66235421+arnav-sachdeva@users.noreply.github.com>
2022-12-31 03:20:13 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./components/Root";
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+ReactDOM.render(<Root />, document.getElementById("root"));
+
+
+
+
+
+
+
+
+
+
+
+
+
